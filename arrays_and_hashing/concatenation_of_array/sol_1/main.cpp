48b94a6eb8995a7eaa1a5a963a2b1e2ee1f8af9e
Offliners <eric21904@gmail.com>
2023-07-27 00:00:40 +0800

    add concatenation_of_array

---
+++ arrays_and_hashing/concatenation_of_array/sol_1/main.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Solution
+{
+    public:
+        vector<int> getConcatenation(vector<int>& nums)
+        {
+            int n = nums.size();
+            vector<int> ans(2 * n);
+            for(int i = 0; i < n; ++i)
+            {
+                ans[i] = nums[i];
+                ans[i + n] = nums[i];
+            }
+
+            return ans;
+        }
+};
+
+
+int main(void)
+{
+    int data;
+    vector<int> nums;
+    Solution sol;
+
+    while(cin >> data)
+        nums.push_back(data);
+
+    vector<int> ans = sol.getConcatenation(nums);
+    for(int i = 0; i < ans.size(); ++i)
+        cout << ans[i] << endl;
+
+    return 0;
+}
